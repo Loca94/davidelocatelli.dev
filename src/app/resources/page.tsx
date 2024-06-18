95c5978cee6673e2f0709362769c1fa75ada3438
Loca94 <davide.loca94@gmail.com>
2024-06-18 21:39:58 +0200

    Created InfiniteScrollingIcons component

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,51 +1,9 @@
 import Link from 'next/link';
 import Container from '@/components/Container';
 import { FadeInStagger, FadeIn } from '@/components/animations/FadeIn';
-import InfiniteScroll from '@/components/animations/InfiniteScroll';
-import {
-  AddressBookTabsIcon,
-  AirplaneIcon,
-  AlarmIcon,
-  AvocadoIcon,
-  BarcodeIcon,
-  BellIcon,
-  BicycleIcon,
-  BluetoothIcon,
-  BoneIcon,
-  BowlSteamIcon,
-  BrainIcon,
-  BroomIcon,
-  BrowsersIcon,
-  CameraIcon,
-  CarrotIcon,
-  ChartBarIcon,
-  ChefHatIcon,
-  CpuIcon,
-  DropIcon,
-  FeatherIcon,
-  FloppyDiskIcon,
-  GameControllerIcon,
-  HamburgerIcon,
-  HandPeaceIcon,
-  IceCreamIcon,
-  LegoIcon,
-  LightningIcon,
-  MapPinIcon,
-  MeteorIcon,
-  MountainIcon,
-  MouseMiddleClickIcon,
-  OnigiriIcon,
-  PenNibIcon,
-  RadioactiveIcon,
-  ThumbsUpIcon,
-  TruckTrailerIcon,
-  VideoConferenceIcon,
-  WavesIcon,
-  WindmillIcon,
-  YarnIcon,
-} from '@/components/Icons';
 import { StackingWebsites } from '@/components/animations/StackingWebsites';
 import { PageDescription, PageTitle } from '@/components/ui/PageLayout';
+import InfiniteScrollingIcons from '@/components/InfiniteScrollingIcons';
 
 export default function ResourcesPage() {
   return (
@@ -70,70 +28,7 @@ export default function ResourcesPage() {
           <FadeIn>
             <Link href="/resources/icons" className="group">
               <div className="relative mb-3 flex h-40 w-full max-w-[calc(100vw-3rem)] flex-col overflow-hidden rounded-lg border border-emerald-300/50 bg-emerald-100 transition duration-300 sm:h-48 sm:max-w-xl dark:border-neutral-700 dark:bg-neutral-900/30">
-                <InfiniteScroll
-                  direction="left"
-                  speed="normal"
-                  pauseOnHover={false}
-                >
-                  <AlarmIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <RadioactiveIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <MapPinIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <LightningIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <DropIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <ThumbsUpIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <ChartBarIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <PenNibIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <MountainIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <WavesIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                </InfiniteScroll>
-                <InfiniteScroll
-                  direction="right"
-                  speed="fast"
-                  pauseOnHover={false}
-                >
-                  <AirplaneIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <CameraIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BellIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <FloppyDiskIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <CarrotIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BluetoothIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <HamburgerIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <IceCreamIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <OnigiriIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BowlSteamIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                </InfiniteScroll>
-                <InfiniteScroll
-                  direction="left"
-                  speed="normal"
-                  pauseOnHover={false}
-                >
-                  <AddressBookTabsIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <AvocadoIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <ChefHatIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <GameControllerIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <HandPeaceIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <MeteorIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <MouseMiddleClickIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <LegoIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <YarnIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <TruckTrailerIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                </InfiniteScroll>
-                <InfiniteScroll
-                  direction="right"
-                  speed="slow"
-                  pauseOnHover={false}
-                >
-                  <VideoConferenceIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <WindmillIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BarcodeIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BoneIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BicycleIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BrainIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BroomIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <BrowsersIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <CpuIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                  <FeatherIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-neutral-400 dark:group-hover:text-emerald-500" />
-                </InfiniteScroll>
+                <InfiniteScrollingIcons />
               </div>
               <div>
                 <h2 className="font-medium text-neutral-900 dark:text-neutral-200">
@@ -148,7 +43,7 @@ export default function ResourcesPage() {
 
           <FadeIn>
             <Link href="/resources/websites" className="group">
-              <div className="relative mb-2 flex h-40 w-full max-w-[calc(100vw-3rem)] justify-center overflow-hidden rounded-lg border border-emerald-300/50 bg-emerald-100 px-4 py-8 sm:h-48 sm:max-w-xl dark:border-neutral-700 dark:bg-neutral-900/30">
+              <div className="relative mb-3 flex h-40 w-full max-w-[calc(100vw-3rem)] justify-center overflow-hidden rounded-lg border border-emerald-300/50 bg-emerald-100 px-4 py-8 sm:h-48 sm:max-w-xl dark:border-neutral-700 dark:bg-neutral-900/30">
                 <StackingWebsites />
               </div>
               <div>
---
+++ src/components/InfiniteScrollingIcons.tsx
@@ -0,0 +1,98 @@
+import InfiniteScroll from '@/components/animations/InfiniteScroll';
+import {
+  AddressBookTabsIcon,
+  AirplaneIcon,
+  AlarmIcon,
+  AvocadoIcon,
+  BarcodeIcon,
+  BellIcon,
+  BicycleIcon,
+  BluetoothIcon,
+  BoneIcon,
+  BowlSteamIcon,
+  BrainIcon,
+  BroomIcon,
+  BrowsersIcon,
+  CameraIcon,
+  CarrotIcon,
+  ChartBarIcon,
+  ChefHatIcon,
+  CpuIcon,
+  DropIcon,
+  FeatherIcon,
+  FloppyDiskIcon,
+  GameControllerIcon,
+  HamburgerIcon,
+  HandPeaceIcon,
+  IceCreamIcon,
+  LegoIcon,
+  LightningIcon,
+  MapPinIcon,
+  MeteorIcon,
+  MountainIcon,
+  MouseMiddleClickIcon,
+  OnigiriIcon,
+  PenNibIcon,
+  RadioactiveIcon,
+  ThumbsUpIcon,
+  TruckTrailerIcon,
+  VideoConferenceIcon,
+  WavesIcon,
+  WindmillIcon,
+  YarnIcon,
+} from '@/components/Icons';
+
+export default function InfiniteScrollingIcons() {
+  return (
+    <>
+      <InfiniteScroll direction="left" speed="normal" pauseOnHover={false}>
+        <AlarmIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <RadioactiveIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <MapPinIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <LightningIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <DropIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <ThumbsUpIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <ChartBarIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <PenNibIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <MountainIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <WavesIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+      </InfiniteScroll>
+      <InfiniteScroll direction="right" speed="fast" pauseOnHover={false}>
+        <AirplaneIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <CameraIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BellIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <FloppyDiskIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <CarrotIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BluetoothIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <HamburgerIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <IceCreamIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <OnigiriIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BowlSteamIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+      </InfiniteScroll>
+      <InfiniteScroll direction="left" speed="normal" pauseOnHover={false}>
+        <AddressBookTabsIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <AvocadoIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <ChefHatIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <GameControllerIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <HandPeaceIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <MeteorIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <MouseMiddleClickIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <LegoIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <YarnIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <TruckTrailerIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+      </InfiniteScroll>
+      <InfiniteScroll direction="right" speed="slow" pauseOnHover={false}>
+        <VideoConferenceIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <WindmillIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BarcodeIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BoneIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BicycleIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BrainIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BroomIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <BrowsersIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <CpuIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+        <FeatherIcon className="shadow-svg size-5 text-emerald-600 transition-colors duration-300 sm:size-6 dark:text-emerald-500 sm:dark:text-neutral-400 sm:dark:group-hover:text-emerald-500" />
+      </InfiniteScroll>
+    </>
+  );
+}
